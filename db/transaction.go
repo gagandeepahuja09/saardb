e00183126524157ab5d45ccdddcd233e25852a47
gagandeepahuja09 <gagandeepahuja09@gmail.com>
2026-09-12 10:33:53 +0530

    remove read locks: readers should not be blocked on writers + update tests for it

--- db/transaction.go
+++ db/transaction.go
@@ -28,7 +28,6 @@ type walPutCommand struct {
 
 func (txn *Transaction) tryAcquireWriteLock(key string) error {
 	locksAcquired, ok := txn.db.transactionManager.keyVsLocksAcquiredMap[key]
-	readLockAlreadyAcquired := false
 	if !ok {
 		locksAcquired = &LocksAcquired{}
 	} else {
@@ -40,60 +39,10 @@ func (txn *Transaction) tryAcquireWriteLock(key string) error {
 		if writerTxnId == txn.id {
 			return nil
 		}
-
-		readerTxnIds := locksAcquired.readerTxnIds
-		if len(readerTxnIds) > 1 {
-			return errors.New(WriteLockNotAcquiredDueToReadLocksError)
-		}
-		if len(readerTxnIds) == 1 {
-			if readerTxnIds[0] == txn.id {
-				readLockAlreadyAcquired = true
-				locksAcquired.readerTxnIds = []uint64{}
-			} else {
-				// todo: need to update this. readers cannot block writers.
-				// this is another learning. thinking about all of the edge case beforehand is pretty
-				// hard. we encounter and think about a lot of cases only during development.
-				return errors.New(WriteLockNotAcquiredDueToReadLocksError)
-			}
-		}
 	}
 	locksAcquired.writerTxnId = txn.id
 	if txn.lockAcquiredKeys == nil {
 		txn.lockAcquiredKeys = []string{}
-	}
-	if !readLockAlreadyAcquired {
-		txn.lockAcquiredKeys = append(txn.lockAcquiredKeys, key)
-	}
-	txn.db.transactionManager.keyVsLocksAcquiredMap[key] = locksAcquired
-	return nil
-}
-
-func (txn *Transaction) tryAcquireReadLock(key string) error {
-	locksAcquired, ok := txn.db.transactionManager.keyVsLocksAcquiredMap[key]
-	writeLockAlreadyAcquired := false
-	if !ok {
-		locksAcquired = &LocksAcquired{}
-	} else {
-		writerTxnId := locksAcquired.writerTxnId
-		if writerTxnId != 0 {
-			if writerTxnId != txn.id {
-				return fmt.Errorf("cannot acquire read lock as write lock acquired by transaction '%d'", writerTxnId)
-			} else {
-				writeLockAlreadyAcquired = true
-			}
-		}
-	}
-	for _, txnId := range locksAcquired.readerTxnIds {
-		if txnId == txn.id {
-			return nil
-		}
-	}
-	locksAcquired.readerTxnIds = append(locksAcquired.readerTxnIds, txn.id)
-	if txn.lockAcquiredKeys == nil {
-		txn.lockAcquiredKeys = []string{}
-	}
-	if !writeLockAlreadyAcquired {
-		txn.lockAcquiredKeys = append(txn.lockAcquiredKeys, key)
 	}
 	txn.db.transactionManager.keyVsLocksAcquiredMap[key] = locksAcquired
 	return nil
@@ -117,11 +66,7 @@ func (txn *Transaction) Put(key, value string) error {
 
 func (txn *Transaction) Get(key string) (string, error) {
 	txn.db.transactionManager.mu.Lock()
-	err := txn.tryAcquireReadLock(key)
 	txn.db.transactionManager.mu.Unlock()
-	if err != nil {
-		return "", err
-	}
 	if value, ok := txn.bufferedWriteMap[key]; ok {
 		return value, nil
 	}
